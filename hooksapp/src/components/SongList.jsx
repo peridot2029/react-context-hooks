6539d11e3bc9cff7e249e5e5321f21209471d410
peridot2029 <peridot2029@gmail.com>
2020-07-12 23:16:01 +0900

    lesson-09 useState Hook

--- hooksapp/src/components/SongList.jsx
+++ hooksapp/src/components/SongList.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import uuid from "uuid/dist/v1";
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    { id: 1, title: "almost home" },
+    { id: 2, title: "memory gospel" },
+    { id: 3, title: "this wild darkness" },
+  ]);
+  const addSong = () => {
+    setSongs([...songs, { id: uuid(), title: "new song" }]);
+  };
   return (
     <div className="song-list">
       <ul>
-        <li>this wild darkness</li>
-        <li>memory gospel</li>
+        {songs.map((song) => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
       </ul>
+      <button onClick={addSong}>Add a song</button>
     </div>
   );
 };
