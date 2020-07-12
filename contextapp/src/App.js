e60735ba2725eb74f303af50d54968599240ef1a
peridot2029 <peridot2029@gmail.com>
2020-07-12 22:01:22 +0900

    lesson-07 Consuming Multiple Contexts

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -5,7 +5,6 @@ import ThemeToggle from "./components/ThemeToggle";
 import AuthContextProvider from "./contexts/AuthContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 
-// context를 중첩해도 지장 없다.
 function App() {
   return (
     <div className="App">
--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,26 +1,38 @@
 import React, { Component } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 import { ThemeContext } from "./../contexts/ThemeContext";
-
+/**
+ * [Consuming Multiple Contexts]
+ *  여러 개의 Contexts 시용할 경우에는 "context"를 호출하지 않는다.
+ *  하나의 주제 문맥이라고 명시해야 한다.
+ */
 class Navbar extends Component {
-  /* 해당 context 객체로 매개변수로 사용하는 함수 */
   render() {
     return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <nav style={{ background: theme.ui, color: theme.syntax }}>
-              <h1>Context App</h1>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-          );
-        }}
-      </ThemeContext.Consumer>
+      <AuthContext.Consumer>
+        {(authContext) => (
+          <ThemeContext.Consumer>
+            {(themeContext) => {
+              const { isAuthentocated, toggleAuth } = authContext;
+              const { isLightTheme, light, dark } = themeContext;
+              const theme = isLightTheme ? light : dark;
+              return (
+                <nav style={{ background: theme.ui, color: theme.syntax }}>
+                  <h1>Context App</h1>
+                  <div onClick={toggleAuth}>
+                    {isAuthentocated ? "Logged in" : "Logged out"}
+                  </div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
