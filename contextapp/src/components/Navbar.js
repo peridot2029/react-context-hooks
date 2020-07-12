30250c35bf756e08d27377e992ec32019bf88cbc
peridot2029 <peridot2029@gmail.com>
2020-07-12 20:10:14 +0900

    lesson-04 accessing context part02

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -2,21 +2,25 @@ import React, { Component } from "react";
 import { ThemeContext } from "./../contexts/ThemeContext";
 
 class Navbar extends Component {
-  static contextType = ThemeContext;
-
+  /* 해당 context 객체로 매개변수로 사용하는 함수 */
   render() {
-    console.log(this.context);
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
