1a44bc14f114d1ff1d40ef73636aabfbe2b38d6e
peridot2029 <peridot2029@gmail.com>
2020-07-18 14:41:18 +0900

    change

---
+++ hooksapp/src/components/NewSongForm.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const NewSongForm = ({ addSong }) => {
+  const [title, setTitle] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("NewSongForm title :", title);
+    addSong(title);
+    setTitle("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Song Name :</label>
+      <input
+        type="text"
+        required
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input type="submit" value="add song" />
+    </form>
+  );
+};
+export default NewSongForm;
